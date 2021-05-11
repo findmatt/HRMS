ff4494f1e81d27762abc0e983637148420ccc4e4
Matthew Chan <find.matt@me.com>
2021-05-11 20:29:25 +0800

    fix navbar class and dropdown event ref

--- src/components/Container/Navbar.js
+++ src/components/Container/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 
 import Dropdown from '../Shared/Dropdown/Dropdown';
@@ -43,34 +43,28 @@ function Navbar(props) {
     ];
     return (
         <div className="lg:container lg:pt-6 mx-auto">
-            <div className="navbar mb-2 shadow-lg bg-neutral-focus lg:rounded-box flex flex-1 justify-between ">
-                <div className="justify-left flex-initial px-2 mx-auto text-neutral-content">
-                    <span className="px-2 mx-2 text-lg font-bold">
-                        <Icon id="FaCross" className="align-middle" />
-                    </span>
-                    <span className="px-2 mx-2 text-lg font-bold ">
-                        Grace Church
-                    </span>
-                </div>
-                <form className="justify-center flex-grow flex-1 px-2 mx-auto text-neutral-content">
-                    <div className="form-control flex-grow">
-                        <input
-                            type="text"
-                            placeholder="Search Members"
-                            className="input input-ghost"
-                        />
-                    </div>
-                    <button type="button" className="btn btn-square btn-ghost">
+            <div
+                className="navbar mb-2 shadow-lg bg-neutral-focus lg:rounded-box text-neutral-content
+                            flex flex-1 justify-between flex-grow"
+            >
+                <span className="px-2 mx-2  text-lg font-bold">Grace</span>
+                <form className="flex flex-1 justify-left flex-grow">
+                    <input
+                        type="text"
+                        placeholder="Search Members"
+                        className="input input-ghost input-group flex-grow"
+                    />
+                    <button
+                        type="button"
+                        className="btn btn-square btn-ghost input-group-append"
+                    >
                         <Icon id="FaSearch" />
                     </button>
                 </form>
-                <div className="justify-end flex-1 px-2  ">
-                    <div className="relative">
-                        <Dropdown menu={menu} />
-                    </div>
+                <div>
+                    <Dropdown menu={menu} toggleIcon="FaBar" />
                 </div>
             </div>
-            {/* <Drawer show="True" /> */}
             {children}
         </div>
     );
--- src/components/Shared/Dropdown/Dropdown.js
+++ src/components/Shared/Dropdown/Dropdown.js
@@ -4,68 +4,67 @@ import { Link } from 'react-router-dom';
 import Icon from '../Icon/Icon';
 
 function Dropdown(props) {
-    const { menu } = props;
-    const [isVisible, setIsVisible] = useState(false);
-    const dropdownRef = useRef();
+    const { menu, toggleIcon } = props;
+    const [visible, setVisible] = useState(false);
 
-    const handleClickOutside = (event) => {
-        if (
-            dropdownRef.current &&
-            !dropdownRef.current.contains(event.target)
-        ) {
-            setIsVisible(!isVisible);
-        }
+    const handleClick = (evt) => {
+        setVisible(!visible);
     };
 
-    const mapMenuGroupItems = (menuItems, groupKey) =>
-        menuItems.map((item, i) => {
-            const optionKey = `${groupKey}${i}`;
-            return (
-                <li key={optionKey}>
-                    <Link
-                        to="/"
-                        onClick={item.handleClick}
-                        className="flex justify-left"
-                    >
-                        <Icon id={item.icon} />
-                        <div className="px-2">{item.text}</div>
-                    </Link>
-                </li>
-            );
-        });
+    const dropdownRef = useRef();
 
-    const mapMenu = (menuGroups) =>
-        menuGroups.map((group, i) => {
-            const groupKey = i;
-            return (
-                <div key={groupKey}>
-                    <li key={groupKey} className="menu-title">
-                        <span>{group.title}</span>
-                    </li>
-                    {mapMenuGroupItems(group.menuItems, groupKey)}
-                </div>
-            );
-        });
+    const handleClickOutside = (evt) => {
+        if (dropdownRef.current && !dropdownRef.current.contains(evt.target)) {
+            setVisible(false);
+        }
+    };
 
     useEffect(() => {
         document.addEventListener('click', handleClickOutside, true);
         return () => {
             document.removeEventListener('click', handleClickOutside, true);
         };
-    }, []);
+    }, [visible]);
+
+    const mapMenuGroupItems = (menuItems) =>
+        menuItems.map((item) => (
+            <li key={item.text} className="px-2 mx-2">
+                <Link
+                    to="/"
+                    onClick={item.handleClick}
+                    className="flex justify-left "
+                >
+                    <Icon id={item.icon} />
+                    <div className="px-2">{item.text}</div>
+                </Link>
+            </li>
+        ));
+
+    const mapMenu = (menuGroups) =>
+        menuGroups.map((group) => (
+            <div>
+                <li key={group.title} className="menu-title">
+                    <span>{group.title}</span>
+                </li>
+                {mapMenuGroupItems(group.menuItems)}
+            </div>
+        ));
+
     return (
-        <div className="relative">
+        <div ref={dropdownRef} className="relative">
             <button
                 type="button"
                 className="btn btn-ghost rounded-btn text-neutral-content"
-                onClick={() => setIsVisible(!isVisible)}
+                onClick={handleClick}
             >
-                <Icon id="FaBar" />
+                <Icon id={toggleIcon} />
             </button>
-            {isVisible && (
+            {visible && (
                 <ul
-                    ref={dropdownRef}
-                    className="menu p-4 shadow-lg bg-base-100 bg-neutral-focus rounded-box absolute w-max float-none right-0 z-2"
+                    className="menu p-4 w-max 
+                            shadow-lg bg-base-100 bg-neutral-focus rounded-box 
+                            text-neutral-focus
+                            absolute float-left right-0"
                 >
                     {mapMenu(menu)}
                 </ul>
@@ -76,6 +75,7 @@ function Dropdown(props) {
 
 Dropdown.propTypes = {
     menu: PropTypes.arrayOf(PropTypes.object),
+    toggleIcon: PropTypes.arrayOf(PropTypes.string),
 };
 
 export default Dropdown;
